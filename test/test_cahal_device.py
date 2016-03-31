b432db37ba3f3e2fd22f815324d932dfd2b1877b
Brent Carrara <bcarr092@uottawa.ca>
2016-03-31 09:46:12 -0400

    Updated CAHAL tests.

--- test/test_cahal_device.py
+++ test/test_cahal_device.py
@@ -90,7 +90,7 @@ class TestsCAHALDevice( unittest.TestCase ):
       global_input_device_name  = "Microphone"
       global_output_device_name = "Speakers"
       global_number_of_channels = 2
-      global_sample_rate        = 48000
+      global_sample_rate        = 44100
       global_bit_depth          = 16
       global_flags              = 0
                                                                                 
